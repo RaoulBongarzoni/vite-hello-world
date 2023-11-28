0d8fb2464afa13dbdd4ecb238df2e480539ddf08
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2023-11-28 10:37:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script >
+
+</script>
+
+<template>
+  <div>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quam delectus amet itaque error id, alias, incidunt dolores, qui nisi fugiat reiciendis rem voluptatem suscipit fuga deserunt soluta! Expedita, dolores!</p>
+  </div>
+  <img src="https://picsum.photos/seed/picsum/300/300" alt="">
+</template>
+
+<style>
+
+</style>
